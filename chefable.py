bef94aee193d5b0282a7f4d050f7247662ccce9c
Hasani <52348811+Hazperera@users.noreply.github.com>
2022-02-01 18:43:43 +0000

    Update chefable.py

--- chefable.py
+++ chefable.py
@@ -8,25 +8,22 @@ import openpyxl
 # 3. retrieve the recipe data json
 
 def recipe_search(ingredient):
-    app_id1 = "d1eb20ae"                              #enter app id
-    app_key1 = "c7f62665307a359fced150d8be8ff42c"     #enter app key
-    result = requests.get(
-        'https://api.edamam.com/search?q={}&app_id={}&app_key={}'
-            .format( ingredient, app_id1, app_key1 )
-    )
+    app_id1 = "enter app id"                              
+    app_key1 = "enter app key"     
+    result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'
+            .format( ingredient, app_id1, app_key1))
     data = result.json()
     return data['hits']
 
 
-
-# Getting started with API data
+#Getting started with API data
 #input ingredient
 ingredient = input('Enter ingredients:')
 
-# call recipe_search()
+#call recipe_search()
 results = recipe_search(ingredient)
 
-# loop retrieved data and extract label,url,totalWeight
+#loop retrieved data and extract label,url,totalWeight
 for result in results:
     recipe = result['recipe']
 
@@ -38,11 +35,10 @@ for result in results:
     print(recipe['totalWeight'])
 
 
-# using Pandas’s built-in function "pd.json_normalize"
-# to normalize json data into a flat table
+#normalize json data into a flat table
 df=pd.json_normalize(results)
 
-# using the to_excel() method to export data as an Excel file via openpyxl library
+#save data
 df.to_excel('chefable.xlsx')
 
 
